1934e4e36b8342d487961f2f42727697dfd267e8
johned <johned.matt@gmail.com>
2016-09-19 18:59:59 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+var express = require ("express");
+var server = express ();
+
+
+  server.get('/', function(request, response){
+    response.sendFile('public/html/index.html', { root: __dirname });
+  });
+
+server.get("/signup", function(request, response){
+  response.sendFile('public/html/signup.html', { root: __dirname });
+});
+
+server.get("/signup", function(request, response){
+  response.sendFile('public/html/login.html', { root: __dirname });
+});
+
+
+  server.listen(8080, function(){
+    console.log('now listening on port...',8080);
+  });
